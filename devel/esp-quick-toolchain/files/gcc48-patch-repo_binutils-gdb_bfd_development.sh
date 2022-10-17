bf8b607946aa385667d9b0008ffbf3afcc959f3a
Tomoyuki Sakurai <y@trombik.org>
2022-10-17 10:44:32 +0700

    breaking: drop gcc48 FLAVOR
    RECOMMENDED_ARDUINO_FRAMEWORK_VERSION is now 3.x in esphome. I do not
    have any project that needs Arduino 2.x.

--- devel/esp-quick-toolchain/files/gcc48-patch-repo_binutils-gdb_bfd_development.sh
+++
@@ -1,8 +0,0 @@
---- repo/binutils-gdb/bfd/development.sh.orig	2021-07-08 07:14:00 UTC
-+++ repo/binutils-gdb/bfd/development.sh
-@@ -16,4 +16,4 @@
- # along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
- # Controls whether to enable development-mode features by default.
--development=true
-+development=false
